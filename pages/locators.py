821618259cf4d303a160f61bfa4d73fd7343da36
Ivan-Petrov-91 <Petrov7678@yandex.ru>
2021-08-07 11:21:27 +0300

    Added new tests

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators():
     PRODUCT_PRICE = (By.CSS_SELECTOR, '[class="price_color"]')
     BASKET_TOTAL = (By.CSS_SELECTOR, '[class="alertinner "] p strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '[id="messages"] [class="alert alert-safe alert-noicon alert-success  fade in"]:nth-child(1)')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
